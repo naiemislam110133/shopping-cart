32e05575141e76fd700574c7829603ed4ee23815
Naiem Islam <naiemislam1547@gmail.com>
2023-04-28 10:09:12 +0600

    added click handler phone number

--- js/phone.js
+++ js/phone.js
@@ -1,1 +1,28 @@
-console.log('phone button is added');
+    function updatePhoneNumber(isIncrese){
+        const phoneNumberField = document.getElementById("phone-number-field");
+        const phoneNumberString = phoneNumberField.value;
+        const previousPhoneNumber = parseInt(phoneNumberString);
+
+        let newPhoneNumber;
+        if (isIncrese) {
+            newPhoneNumber = previousPhoneNumber + 1 ;
+        }
+        else{
+            newPhoneNumber = previousPhoneNumber - 1;
+        }
+       
+        phoneNumberField.value = newPhoneNumber;
+        return newPhoneNumber;
+
+    };
+
+
+document.getElementById("phone-plus-btn").addEventListener("click", function(){
+   const newPhoneNumber = updatePhoneNumber(true);     
+    
+  
+});
+
+document.getElementById("phone-minus-btn").addEventListener("click", function(){
+    const newPhoneNumber = updatePhoneNumber(false);
+});
